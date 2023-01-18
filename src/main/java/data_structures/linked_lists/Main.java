bf17259fdfe8bfeabce259846cb9176cc1ffbd58
E. Hannah <echannah631@gmail.com>
2023-01-18 16:10:27 -0600

    adding linked list initial code

---
+++ src/main/java/data_structures/linked_lists/Main.java
@@ -0,0 +1,11 @@
+package data_structures.linked_lists;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(1);
+
+        myLinkedList.append(2);
+
+        myLinkedList.printList();
+    }
+}
