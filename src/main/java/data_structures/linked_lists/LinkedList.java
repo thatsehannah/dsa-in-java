a9026e2eddedc3b063986e75c75974a2c705d825
E. Hannah <echannah631@gmail.com>
2023-01-18 17:02:13 -0600

    get Node at index logic

--- src/main/java/data_structures/linked_lists/LinkedList.java
+++ src/main/java/data_structures/linked_lists/LinkedList.java
@@ -111,4 +111,17 @@ public class LinkedList {
 
         return temp;
     }
+
+    public Node get (int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+
+        return temp;
+    }
 }
--- src/main/java/data_structures/linked_lists/Main.java
+++ src/main/java/data_structures/linked_lists/Main.java
@@ -2,13 +2,15 @@ package data_structures.linked_lists;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList myLinkedList = new LinkedList(2);
+        LinkedList myLinkedList = new LinkedList(0);
         myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
 
-        System.out.println(myLinkedList.removeFirst().value);
+        System.out.println(myLinkedList.get(2).value);
+
         myLinkedList.printList();
-        System.out.println(myLinkedList.removeFirst().value);
-        myLinkedList.printList();
-        System.out.println(myLinkedList.removeFirst());
+
+
     }
 }
