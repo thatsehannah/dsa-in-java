d9b4aeaf6db252658444b28fb7b524b1633589c2
E. Hannah <echannah631@gmail.com>
2023-01-19 11:10:13 -0600

    removing the tail Node in doubly linked list

--- src/main/java/data_structures/doubly_linked_lists/DoublyLinkedList.java
+++ src/main/java/data_structures/doubly_linked_lists/DoublyLinkedList.java
@@ -58,4 +58,24 @@ public class DoublyLinkedList {
         tail = newNode;
         length++;
     }
+
+    public Node removeLast() {
+        if (length == 0) {
+            return null;
+        }
+
+        Node temp = tail;
+
+        if (length == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+            temp.prev = null;
+        }
+
+        length--;
+        return temp;
+    }
 }
--- src/main/java/data_structures/doubly_linked_lists/Main.java
+++ src/main/java/data_structures/doubly_linked_lists/Main.java
@@ -4,7 +4,15 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList myDLL = new DoublyLinkedList(7);
         myDLL.append(8);
+        myDLL.printList();
 
+        myDLL.removeLast();
+        myDLL.printList();
+
+        myDLL.removeLast();
+        myDLL.printList();
+
+        myDLL.removeLast();
         myDLL.printList();
     }
 }
