8e5d1c475ebcb9cb26ae51d1eac67cefc5428152
E. Hannah <echannah631@gmail.com>
2023-01-20 16:08:30 -0600

    remove edges in graph

--- src/main/java/data_structures/graphs/Main.java
+++ src/main/java/data_structures/graphs/Main.java
@@ -5,8 +5,13 @@ public class Main {
         Graph myGraph = new Graph();
         myGraph.addVertex("A");
         myGraph.addVertex("B");
+        myGraph.addVertex("C");
 
         myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("B", "C");
+        myGraph.printGraph();
+        myGraph.removeEdge("A", "B");
         myGraph.printGraph();
     }
 }
